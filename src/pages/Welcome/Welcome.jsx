8f2e22ad0343eaf9b0252d06cf166d731612c6f8
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-03-25 12:35:47 -0600

    Welcome page
    Responsive done

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -7,15 +7,19 @@ const Welcome = () => {
   return (
     <div className="welcome">
       <h1 className="main">WAPI</h1>
-      <img src={logo} alt="" className="logo" />
-      <div className="wrapper">
-        <h1 className="title">Comenzar</h1>
-        <div className="botones">
-          <button className="btnLogin">Inicia sesion</button>
-          <button className="btnSign">Registrarse</button>
+      <div className="welcomeContainer">
+        <div className="logoContainer">
+          <h2>Viaja fácil y rapido con flecha amarilla</h2>
+          <img src={logo} alt="" className="logo" />
+        </div>
+        <div className="wrapper">
+          <h1 className="title">Comenzar</h1>
+          <div className="botones">
+            <button className="btnLogin">Inicia sesion</button>
+            <button className="btnSign">Registrarse</button>
+          </div>
         </div>
       </div>
-      <div className="bottom"></div>
     </div>
   );
 };
