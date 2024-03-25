03f6e1cf7a6e2c8aae8ed920bca802d2fce40591
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-03-25 12:02:04 -0600

    Add logo
    Logo and welcome page

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./welcome.scss";
 
+import logo from "../../assets/logo.png";
+
 const Welcome = () => {
   return (
     <div className="welcome">
+      <h1 className="main">WAPI</h1>
+      <img src={logo} alt="" className="logo" />
       <div className="wrapper">
-        <h1 className="title">Viaje facil y sencillo con flecha amarilla</h1>
+        <h1 className="title">Comenzar</h1>
         <div className="botones">
           <button className="btnLogin">Inicia sesion</button>
-          <button className="btnLogin">Inicia sesion</button>
+          <button className="btnSign">Registrarse</button>
         </div>
       </div>
+      <div className="bottom"></div>
     </div>
   );
 };
